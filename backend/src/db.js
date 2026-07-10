2fbb87c4fb324c84ec2bb4569a1162a6a4df3cea
StreamingBox <cuentastrbx@gmail.com>
2026-07-09 19:13:13 -0500

    Prefer IPv4 for database DNS

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,8 @@
+const dns = require("node:dns");
 const mysql = require("mysql2/promise");
 const { buildDbConfig } = require("./utils/dbConfig");
+
+dns.setDefaultResultOrder(process.env.DB_DNS_RESULT_ORDER || "ipv4first");
 
 const pool = mysql.createPool(buildDbConfig());
 
